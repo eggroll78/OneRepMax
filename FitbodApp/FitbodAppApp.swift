4cff56ac4dae2514c1e29c30c952e23e89a2e4cf
Cliff Chen <cliffc@nextdoor.com>
2023-03-09 09:57:42 -0800

    Initial Commit

---
+++ FitbodApp/FitbodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitbodAppApp.swift
+//  FitbodApp
+//
+//  Created by Cliff Chen on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct FitbodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
